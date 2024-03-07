b7f583004fd28d217a0b468d8fdf04102a74b7a3
Holden <holden@rdstudios.org>
2024-03-06 21:37:32 -0800

    Customization

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -74,15 +74,26 @@
   # FIXME: Add the rest of your current configuration
 
   # TODO: Set your hostname
-  networking.hostName = "your-hostname";
+  networking.hostName = "NixOS";
 
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.device = "nodev";
+
+  services.xserver.desktopManager.plasma5 = {
+    enable = true;
+  };
+
+  services.xserver.displayManager.sddm = {
+    enable = true;
+  };
+
+  services.xserver.enable = true;
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     # FIXME: Replace with your username
-    your-username = {
+    holden = {
       # TODO: You can set an initial password for your user.
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
