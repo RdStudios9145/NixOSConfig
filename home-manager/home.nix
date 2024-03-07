a2999fb97316cdf188a48919121f367b34663286
Holden <holden@rdstudios.org>
2024-03-07 08:34:46 -0800

    neovim version clash

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -55,7 +55,7 @@
 
   # Add stuff for your user as you see fit:
   programs.neovim.enable = true;
-  home.packages = with pkgs; [ steam neovim ];
+  home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
