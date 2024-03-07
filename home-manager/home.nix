830b2d89e7a0562f28dd98320cb2ab18446e87de
Holden <holden@rdstudios.org>
2024-03-07 10:19:06 -0800

    stuff

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -56,7 +56,10 @@
   # Add stuff for your user as you see fit:
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
-  home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    steam
+    blender
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
