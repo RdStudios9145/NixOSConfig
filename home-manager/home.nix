ea8e03df513994c65a0edfaf780725d96bbd0791
Holden <holden@rdstudios.org>
2024-03-07 09:26:36 -0800

    some changes

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -55,11 +55,12 @@
 
   # Add stuff for your user as you see fit:
   programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
   home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  # programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
