b9d7da8f5279965d97e3a5ae35f87aa06ae24aea
Holden <holden@rdstudios.org>
2024-03-24 10:03:20 -0700

    still trying

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -110,7 +110,7 @@
   };
 
   i18n = {
-    supportedLocales = [ "en_US.UTF-8" "en_SE.UTF-8" ];
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "en_SE.UTF-8/UTF-8" ];
     extraLocaleSettings = {
       LC_TIME = "en_SE.UTF-8";
     };
