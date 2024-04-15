0d6991ffa138eb8d2428cb09267d9cf405c1c05d
Holden <holden@rdstudios.org>
2024-04-15 12:56:03 -0700

    fish and zsh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -147,4 +147,9 @@
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
+
+  home-manager.users.holden = {
+    programs.fish.enable = true;
+    programs.zsh.enable = true;
+  };
 }
